4a69dec98e0cef6b2bf0267a584ee2fe959ad5e1
David Nidever <dnidever@montana.edu>
2025-09-15 00:22:38 -0600

    updates to setup files

--- setup.py
+++ setup.py
@@ -2,31 +2,20 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
-extensions = [
+ext_modules = [
     Extension(
-        "numcy.fast_array_nd",
+        name="numcy.fastarray",
         sources=["numcy/fastarray.pyx"],
         include_dirs=[np.get_include()],
-        libraries=["blas"],  # or ["openblas"] depending on your system
-        library_dirs=[],
-        extra_compile_args=["-O3"],
+        language="c",
     ),
-    Extension(
-        "numcy.utils",
-        sources=["numcy/utils.pyx"],
-        include_dirs=[np.get_include()],
-        extra_compile_args=["-O3"],
-    )
 ]
 
 setup(
     name="numcy",
     version="0.1.0",
-    description="Fast Cython-based NumPy-like arrays for high-performance computing",
-    author="Your Name",
+    description="Fast array operations in Cython",
     packages=["numcy"],
-    ext_modules=cythonize(extensions, annotate=True, 
-                          compiler_directives={'language_level': 3}),
-    zip_safe=False,
+    ext_modules=cythonize(ext_modules, annotate=True, language_level=3),
+    python_requires=">=3.8",
 )
-
